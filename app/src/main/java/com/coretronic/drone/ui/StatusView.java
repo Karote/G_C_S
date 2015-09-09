50e9976d1e19dc6d537b05fc5085cfa0bd33b8ed
karot.chuang <karoterika@gmail.com>
2015-09-09 16:42:13 +0800

    Update status view. Add communication light, gps indicator and rf indicator.

--- app/src/main/java/com/coretronic/drone/ui/StatusView.java
+++ app/src/main/java/com/coretronic/drone/ui/StatusView.java
@@ -15,10 +15,27 @@ import com.coretronic.drone.R;
  * Created by jiaLian on 15/6/9.
  */
 public class StatusView extends LinearLayout {
-    private ImageView wifiStatus;
+    private static final int[] GPS_RESOURCE_ID = {
+            R.drawable.icon_indicator_status_gps_00,
+            R.drawable.icon_indicator_status_gps_01,
+            R.drawable.icon_indicator_status_gps_02,
+            R.drawable.icon_indicator_status_gps_03,
+            R.drawable.icon_indicator_status_gps_04,
+            R.drawable.icon_indicator_status_gps_05
+    };
+    private static final int[] RF_RESOURCE_ID = {
+            R.drawable.icon_indicator_status_rf_00,
+            R.drawable.icon_indicator_status_rf_01,
+            R.drawable.icon_indicator_status_rf_02,
+            R.drawable.icon_indicator_status_rf_03,
+            R.drawable.icon_indicator_status_rf_04,
+            R.drawable.icon_indicator_status_rf_05
+    };
     private ImageView gpsStatus;
+    private ImageView rfStatus;
     private ProgressBar batteryProgress;
     private TextView tvBattery;
+    private TextView gpsNumber;
 
     public StatusView(Context context) {
         super(context);
@@ -35,15 +52,21 @@ public class StatusView extends LinearLayout {
 
     private void initView() {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.status_view, null);
-        wifiStatus = (ImageView) view.findViewById(R.id.iv_wifi);
         gpsStatus = (ImageView) view.findViewById(R.id.iv_gps);
+        gpsNumber = (TextView) view.findViewById(R.id.tv_gps);
+        rfStatus = (ImageView) view.findViewById(R.id.iv_rf);
         batteryProgress = (ProgressBar) view.findViewById(R.id.progress_battery);
         tvBattery = (TextView) view.findViewById(R.id.tv_battery);
         addView(view);
     }
 
-    public void setWifiStatus(int resid) {
-        wifiStatus.setBackgroundResource(resid);
+    public void setRFStatus(int resid) {
+        rfStatus.setBackgroundResource(RF_RESOURCE_ID[resid]);
+    }
+
+    public void setGpsStatus(int resid){
+        gpsStatus.setBackgroundResource(GPS_RESOURCE_ID[resid]);
+        gpsNumber.setText(String.valueOf(resid));
     }
 
     public void setGpsVisibility(int visibility) {
